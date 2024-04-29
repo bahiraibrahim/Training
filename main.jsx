1b6d02523bdbacfdcf5de5f82bb98713821c9acd
bahiraibrahim <165313256+bahiraibrahim@users.noreply.github.com>
2024-04-29 18:37:14 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home/home.jsx";
+import Html from "./Pages/html/Html.jsx";
+import Css from "./Pages/Css/Css.jsx";
+import JavaScript from "./Pages/JavaScript/JavaScript.jsx";
+
+const router=createBrowserRouter([
+  {
+path:'/',
+element:<Home/>,
+errorElement:<h1>Sorry ...................</h1>
+  },
+  {
+path:'/html',
+element:<Html/>,
+  },
+  {
+path:'/css',
+element:<Css/>,
+  },
+  {
+path:'/javascript',
+element:<JavaScript/>,
+  },
+])
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>
 );
